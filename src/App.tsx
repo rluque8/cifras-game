45ea146b5d0145a535ab3f5d3546de0e4df492be
Rodrigo <rafarod8@gmail.com>
2022-04-23 19:13:31 +0200

    feat: installed tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="mx-auto w-96">
+      <h1 className="text-4xl text-center">Cifras</h1>
+    </div>
+  )
+}
+
+export default App
